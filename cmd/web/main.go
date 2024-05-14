c81864ed779670d5e3cfd87b3fe82e0a3fcc038a
sudhir meena <sureshmeena512@gmail.com>
2024-05-14 09:21:44 +0530

    add changes to main package and handlers

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 
+	"github.com/alexedwards/scs/v2"
 	"github.com/sudhir512kj/go-hello-web/pkg/config"
 	"github.com/sudhir512kj/go-hello-web/pkg/handlers"
 	"github.com/sudhir512kj/go-hello-web/pkg/render"
@@ -11,9 +14,21 @@ import (
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 // main is the entry point for the application
 func main() {
-	var app config.AppConfig
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
@@ -28,9 +43,17 @@ func main() {
 
 	render.NewTemplates(&app)
 
-	http.HandleFunc("/", handlers.Repo.Home)
-	http.HandleFunc("/about", handlers.Repo.About)
+	// http.HandleFunc("/", handlers.Repo.Home)
+	// http.HandleFunc("/about", handlers.Repo.About)
 
 	fmt.Printf("Starting application on port %s", portNumber)
-	_ = http.ListenAndServe(portNumber, nil)
+	// _ = http.ListenAndServe(portNumber, nil)
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/sudhir512kj/go-hello-web
 
 go 1.21.1
+
+require github.com/go-chi/chi/v5 v5.0.12
+
+require (
+	github.com/alexedwards/scs/v2 v2.8.0 // indirect
+	github.com/justinas/nosurf v1.1.1 // indirect
+)
